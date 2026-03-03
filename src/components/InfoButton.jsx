566173a9aadd8e6992a6e27820741f0b257ef0d3
diak <diak@mesterlevel.lo>
2026-03-03 12:17:52 +0100

    firstpage keszen van

--- src/components/InfoButton.jsx
+++ src/components/InfoButton.jsx
@@ -3,8 +3,8 @@ export default function InfoGomb({ src, onClick }) {
         <div>
             <img src={src} alt="info" onClick={onClick}
             style={{
-                width: '45px',
-                height: '45px',
+                width: '67.5px',
+                height: '67.5px',
                 cursor: 'pointer'
             }}/>
         </div>
