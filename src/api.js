5bf3d61f1925e5f9b0167b980ff8f1e2b3eef6d3
diak <diak@mesterlevel.lo>
2026-03-05 09:41:45 +0100

    loginsignup

--- src/api.js
+++ src/api.js
@@ -1,24 +1,60 @@
 const BASE = 'http://localhost:3000'
 
-export async function regisztracio(email, felhasznalonev, jelszo){
+export async function regisztracio(email, felhasznalonev, jelszo) {
     const res = await fetch(`${BASE}/regisztracio`, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
-        body: JSON.stringify({email, felhasznalonev, jelszo, admin: 0})
+        body: JSON.stringify({ email, felhasznalonev, jelszo, admin: 0 })
     })
-    const data= await res.json();
-    if(!res.ok) return {result: false, message: data.message};
-    else return {result: true, message: data.message};
+    const data = await res.json();
+    if (!res.ok) return { result: false, message: data.message };
+    else return { result: true, message: data.message };
 }
-export async function belepes(felhasznalonevVagyEmail, jelszo){
+export async function belepes(felhasznalonevVagyEmail, jelszo) {
     const res = await fetch(`${BASE}/belepes`, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
-        body: JSON.stringify({felhasznalonevVagyEmail, jelszo})
+        body: JSON.stringify({ felhasznalonevVagyEmail, jelszo })
     })
-    const data= await res.json();
-    if(!res.ok) return {result: false, message: data.message};
-    else return {result: true, message: data.message};
+    const data = await res.json();
+    if (!res.ok) return { result: false, message: data.message };
+    else return { result: true, message: data.message };
+}
+export async function kijelentkezes() {
+    const res = await fetch(`${BASE}/kijelentkezes`, {
+        method: 'POST',
+        credentials: 'include'
+    })
+    const data = await res.json()
+    if (!res.ok) return { result: false, message: data.message }
+    else return { result: true, message: data.message }
+}
+
+export async function adataim() {
+    const res = await fetch(`${BASE}/adataim`, {
+        credentials: 'include'
+    })
+    const data = await res.json()
+    if (!res.ok) return { result: false, message: data.message }
+    else return { result: true, data }
+}
+
+export async function getDecks() {
+    const res = await fetch(`${BASE}/decks`, {
+        credentials: 'include'
+    })
+    const data = await res.json()
+    if (!res.ok) return { result: false, message: data.message }
+    else return { result: true, data }
+}
+
+export async function getKartyak() {
+    const res = await fetch(`${BASE}/kartyak`, {
+        credentials: 'include'
+    })
+    const data = await res.json()
+    if (!res.ok) return { result: false, message: data.message }
+    else return { result: true, data }
 }
